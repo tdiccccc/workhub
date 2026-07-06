c1aedf5d9ec5a3a1f8a6564d25570b1a5ed99fa8
daichi taguchi <the.earth3121@gmail.com>
2026-07-06 16:18:36 +0000

    chore: initialize Spring Boot development environment

---
+++ backend/src/main/java/com/taguhyung/workhub/WorkhubApplication.java
@@ -0,0 +1,13 @@
+package com.taguhyung.workhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/taguhyung/workhub/WorkhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taguhyung.workhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
