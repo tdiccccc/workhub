d95f42034c5bddc020f9552f8b74f3779663060a
daichi taguchi <the.earth3121@gmail.com>
2026-07-08 14:48:35 +0000

    feat: error handling

--- backend/src/main/java/com/taguhyung/workhub/presentation/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/taguhyung/workhub/presentation/exception/GlobalExceptionHandler.java
@@ -1,20 +1,48 @@
 package com.taguhyung.workhub.presentation.exception;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import com.taguhyung.workhub.domain.exception.ProjectNotFoundException;
 import com.taguhyung.workhub.presentation.response.ApiResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * プロジェクトが見つからないときの例外
+     * 
+     * @param exception
+     * @return
+     */
     @ExceptionHandler(ProjectNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleProjectNotFoundException(
             ProjectNotFoundException exception) {
         return ResponseEntity
                 .status(404)
                 .body(new ApiResponse<>(404, exception.getMessage(), null));
+    }
+
+    /**
+     * バリデーション時の例外
+     * 
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationException(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ApiResponse<>(400, "Validation failed", errors));
     }
 }
